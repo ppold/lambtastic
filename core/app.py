7b8470dfcb0f940a18d1bbd750987c735159b151
Giorgio Leveroni <giorgio.m.leveroni@gmail.com>
2013-09-21 18:24:13 -0500

    Jinja added.

--- core/app.py
+++ core/app.py
@@ -3,12 +3,15 @@
 
 """ views definitions """
 
-from bottle import app, request, response, get, post, redirect
+import functools
+from os.path import join, dirname, abspath
+import simplejson as json
+from bottle import jinja2_template as template
+from bottle import app, request, response, get, post, redirect, static_file, route
 from sqlalchemy import create_engine
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.sql.expression import desc
 import bottle_sqlalchemy
-import simplejson as json
 # from .models import *
 import settings
 
@@ -21,10 +24,19 @@ plugin = bottle_sqlalchemy.Plugin(engine, keyword="session", use_kwargs=True)
 app().install(plugin)
 
 
+directory = dirname(__file__)
+TEMPLATE_PATH = abspath(join(directory, 'templates'))
+STATIC_PATH = abspath(join(directory, 'static'))
+AVATARS_PATH = abspath(join(directory, 'avatars'))
+
+template = functools.partial(template, template_lookup=[TEMPLATE_PATH])
+
+
 @get("/test")
 def test(session):
     """ the whole test """
 
     response.content_type = "application/json"
     return json.dumps({})
+    # return template('index.html')
 
