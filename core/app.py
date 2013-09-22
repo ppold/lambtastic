d9a9a63d9682c72643bec4ee09427816b3522211
Giorgio Leveroni <giorgio.m.leveroni@gmail.com>
2013-09-21 21:27:33 -0500

    More models defined.

--- core/app.py
+++ core/app.py
@@ -12,7 +12,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.sql.expression import desc
 import bottle_sqlalchemy
-# from .models import *
+from .models import Landmark, Kind
 import settings
 
 # app setup
@@ -27,16 +27,41 @@ app().install(plugin)
 directory = dirname(__file__)
 TEMPLATE_PATH = abspath(join(directory, 'templates'))
 STATIC_PATH = abspath(join(directory, 'static'))
-AVATARS_PATH = abspath(join(directory, 'avatars'))
 
 template = functools.partial(template, template_lookup=[TEMPLATE_PATH])
 
+@get('/')
+def index():
+    """ landing page """
+    return template('index.html')
 
-@get("/test")
-def test(session):
-    """ the whole test """
+@get('/static/<filepath:path>')
+def send_static(filepath):
+    """ serves the front-end resources """
+    return static_file(filepath, root=STATIC_PATH)
+
+@get("/landmarks/<key:int>")
+def landmark(session):
+    """ returns information about a landmark in particular """
 
     response.content_type = "application/json"
-    return json.dumps({})
-    # return template('index.html')
+    data = session.query(Landmark).get(key)
+    return json.dumps(data)
 
+@get("/landmarks/museums")
+def museums(session):
+    """ returns a list of museums """
+
+    response.content_type = "application/json"
+    museums_kind = session.query(Kind).filter(name="museum").one()
+    data = session.query(Landmark).filter(kind=museums_kind).all()
+    return json.dumps(data)
+
+@get("/landmarks/bikeways")
+def bikeways(session):
+    """ returns a list of bikeways """
+
+    response.content_type = "application/json"
+    bikeways_kind = session.query(Kind).filter(name="bikeways").one()
+    data = session.query(Landmark).filter(kind=bikeways_kind).all()
+    return json.dumps(data)
--- core/models.py
+++ core/models.py
@@ -1,16 +1,35 @@
 """ model definitions """
 
-from sqlalchemy import Column, Integer, Text, Date, String
+from sqlalchemy import Column, Integer, Date, Unicode, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.schema import ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 BASE = declarative_base()
 
-
-class Example(BASE):
-    """ model example """
-
-    __tablename__ = "example"
-
+class PKMixin:
+    """ primary key mixin for the rest of the models """
     key = Column(Integer, primary_key=True)
+
+
+class EnumMixin:
+    """ base model for enumerated values """
+
+    name = Column(Unicode, nullable=False)
+    order = Column(Integer, autoincrement=True)
+
+
+class Kind(BASE, PKMixin, EnumMixin):
+    """ Kind of landmark, like a museum, historic site, etc """
+
+    __tablename__ = "kind"
+
+
+class Landmark(BASE, PKMixin, EnumMixin):
+    """ a point of interest """
+
+    __tablename__ = "landmark"
+
+    latitude = Column(String)
+    longitude = Column(String)
+    kind = relationship(Kind, uselist=False)
