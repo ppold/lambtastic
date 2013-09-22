c375c0063a4a0bd159cb2f4cbf22412f52c35cbb
Giorgio Leveroni <giorgio.m.leveroni@gmail.com>
2013-09-21 22:51:37 -0500

    Google map now points to the position of the client.

--- core/static/scripts/app.js
+++ core/static/scripts/app.js
@@ -1,10 +1,24 @@
-function initialize() {
+function initialize(latitude, longitude) {
 	var mapOptions = {
-		center: new google.maps.LatLng(-34.397, 150.644),
-		zoom: 8,
+		center: new google.maps.LatLng(latitude, longitude),
+		zoom: 14,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var map = new google.maps.Map(document.querySelector(".map-canvas"), mapOptions);
 }
-initialize();
-// google.maps.event.addDomListener(window, 'load', initialize);
+
+var response = function response(position) {
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+
+
+	initialize(latitude, longitude);
+}
+
+var get_location = function get_location() {
+	navigator.geolocation.getCurrentPosition(response);
+}
+
+get_location();
+
+
